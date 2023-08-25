1b9d25ff9ba59d75d68b47dfd3ce610b5e141dc5
Marius Storhaug <marstor@hotmail.com>
2023-08-25 02:43:12 +0200

    Add Uninstall-Pester

--- src/Utilities/Utilities.psm1
+++ src/Utilities/Utilities.psm1
@@ -880,7 +880,7 @@ function Reinstall-Module {
     }
 
     $modules = Get-GalleryModule | Where-Object Name -Like "$Name"
-    Write-Verbose 
+    Write-Verbose
 
     $modules | ForEach-Object {
         Uninstall-Module -Name $_ -AllVersions -Force -ErrorAction SilentlyContinue
@@ -889,6 +889,63 @@ function Reinstall-Module {
     $modules.Name | ForEach-Object {
         Install-Module -Name $_ -Scope CurrentUser -Force
     }
+}
+
+<#
+.SYNOPSIS
+Uninstall Pester 3 from Program Files and Program Files (x86)
+
+.DESCRIPTION
+Uninstall Pester 3 from Program Files and Program Files (x86). This is useful
+when you want to install Pester 4 and you have Pester 3 installed.
+
+.PARAMETER All
+Completely remove all built-in Pester 3 installations
+
+.EXAMPLE
+Uninstall-Pester
+
+Uninstall Pester 3 from Program Files and Program Files (x86).
+
+.EXAMPLE
+Uninstall-Pester -All
+
+Completely remove all built-in Pester 3 installations.
+
+#>
+function Uninstall-Pester {
+    [CmdletBinding()]
+    param (
+        # Completely remove all built-in Pester 3 installations
+        [Parameter()]
+        [switch] $All
+    )
+
+    $pesterPaths = foreach ($programFiles in ($env:ProgramFiles, ${env:ProgramFiles(x86)})) {
+        $path = "$programFiles\WindowsPowerShell\Modules\Pester"
+        if ($null -ne $programFiles -and (Test-Path $path)) {
+            if ($All) {
+                Get-Item $path
+            } else {
+                Get-ChildItem "$path\3.*"
+            }
+        }
+    }
+
+    if (-not $pesterPaths) {
+        "There are no Pester$(if (-not $all) {' 3'}) installations in Program Files and Program Files (x86) doing nothing."
+        return
+    }
+
+    foreach ($pesterPath in $pesterPaths) {
+        takeown /F $pesterPath /A /R
+        icacls $pesterPath /reset
+        # grant permissions to Administrators group, but use SID to do
+        # it because it is localized on non-us installations of Windows
+        icacls $pesterPath /grant '*S-1-5-32-544:F' /inheritance:d /T
+        Remove-Item -Path $pesterPath -Recurse -Force -Confirm:$false
+    }
+
 }
 
 Export-ModuleMember -Function '*' -Cmdlet '*' -Alias '*' -Variable '*'
