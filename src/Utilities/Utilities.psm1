822cfa637a3ec38fc4aaba97d493717f06dcad93
Marius Storhaug <marstor@hotmail.com>
2023-08-25 02:03:31 +0200

    Add Get-GalleryModule, Prune and reinstall module

--- src/Utilities/Utilities.psm1
+++ src/Utilities/Utilities.psm1
@@ -820,5 +820,75 @@ function Show-FileExtension {
     }
 }
 
+function Get-GalleryModule {
+    Get-Module -ListAvailable -Refresh | Where-Object RepositorySourceLocation -Match 'https://www.powershellgallery.com/api/v2'
+}
+
+function Prune-Module {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $false)]
+        [string[]] $Name = '*',
+        [Parameter(Mandatory = $false)]
+        [ValidateSet('CurrentUser', 'AllUsers')]
+        [string[]] $Scope = 'CurrentUser'
+    )
+
+    if ($Scope -eq 'AllUsers' -and -not (IsAdmin)) {
+        throw "Administrator rights are required to uninstall modules for all users. Please run the command again with elevated rights (Run as Administrator) or provide '-Scope CurrentUser' to your command."
+    }
+
+    $UpdateableModules = Get-GalleryModule | Where-Object Name -Like "$Name"
+    $UpdateableModuleNames = $UpdateableModules.Name | Sort-Object -Unique
+    foreach ($UpdateableModuleName in $UpdateableModuleNames) {
+        $UpdateableModule = $UpdateableModules | Where-Object Name -EQ $UpdateableModuleName | Sort-Object -Property Version -Descending
+        Write-Verbose "[$($UpdateableModuleName)] - Found [$($UpdateableModule.Count)]" -Verbose
+
+        $NewestModule = $UpdateableModule | Select-Object -First 1
+        Write-Verbose "[$($UpdateableModuleName)] - Newest [$($NewestModule.Version -join ', ')]" -Verbose
+
+        $OutdatedModules = $UpdateableModule | Select-Object -Skip 1
+        Write-Verbose "[$($UpdateableModuleName)] - Outdated [$($OutdatedModules.Version -join ', ')]" -Verbose
+
+        foreach ($OutdatedModule in $OutdatedModules) {
+            Write-Verbose "[$($UpdateableModuleName)] - [$($OutdatedModule.Version)] - Removing" -Verbose
+            $OutdatedModule | Remove-Module -Force
+            Write-Verbose "[$($UpdateableModuleName)] - [$($OutdatedModule.Version)] - Uninstalling" -Verbose
+            Uninstall-Module -Name $OutdatedModule.Name -RequiredVersion -Force
+            try {
+                $OutdatedModule.ModuleBase | Remove-Item -Force -Recurse -ErrorAction Stop
+            } catch {
+                Write-Warning "[$($UpdateableModuleName)] - [$($OutdatedModule.Version)] - Failed to remove [$($OutdatedModule.ModuleBase)]"
+                continue
+            }
+        }
+    }
+}
+
+function Reinstall-Module {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $false)]
+        [string[]] $Name = '*',
+        [Parameter(Mandatory = $false)]
+        [ValidateSet('CurrentUser', 'AllUsers')]
+        [string[]] $Scope = 'CurrentUser'
+    )
+
+    if ($Scope -eq 'AllUsers' -and -not (IsAdmin)) {
+        throw "Administrator rights are required to uninstall modules for all users. Please run the command again with elevated rights (Run as Administrator) or provide '-Scope CurrentUser' to your command."
+    }
+
+    $modules = Get-GalleryModule | Where-Object Name -Like "$Name"
+    Write-Verbose 
+
+    $modules | ForEach-Object {
+        Uninstall-Module -Name $_ -AllVersions -Force -ErrorAction SilentlyContinue
+    }
+
+    $modules.Name | ForEach-Object {
+        Install-Module -Name $_ -Scope CurrentUser -Force
+    }
+}
 
 Export-ModuleMember -Function '*' -Cmdlet '*' -Alias '*' -Variable '*'
