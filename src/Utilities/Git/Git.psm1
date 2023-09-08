2fcaabd318d88ea5b6f5771057d3446a1b9a29e3
Marius Storhaug <marstor@hotmail.com>
2023-09-08 19:43:49 +0200

    Added Git utilities submodule

---
+++ src/Utilities/Git/Git.psm1
@@ -0,0 +1,6 @@
+﻿function Sync-Repo {
+    git checkout main
+    git pull
+    git remote update origin --prune
+    git branch -vv | Select-String -Pattern ': gone]' | ForEach-Object { $_.toString().Trim().Split(' ')[0] } | ForEach-Object { git branch -D $_ }
+}
