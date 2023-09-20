be6fcf3bf754b2d0064decc97bf499cfc3e2c928
Marius Storhaug <marstor@hotmail.com>
2023-09-20 02:20:00 +0200

    Add save and restore for PScredentials

---
+++ src/Utilities/public/PowerShell/PSCredential/New-PSCredential.ps1
@@ -0,0 +1,46 @@
+﻿function New-PSCredential {
+    <#
+        .SYNOPSIS
+        Creates a PSCredential
+
+        .DESCRIPTION
+        Takes in a UserName and a plain text password and creates a PSCredential
+
+        .EXAMPLE
+        New-PSCredential -UserName "Admin" -Password "P@ssw0rd!"
+
+        This creates a PSCredential with username "Admin" and password "P@ssw0rd!"
+
+        .EXAMPLE
+        New-PSCredential -UserName "Admin" -Password (Read-Host -Prompt "Enter Password" -AsSecureString)
+
+        This creates a PSCredential with username "Admin" and password by prompting the user for the password
+
+        .EXAMPLE
+        $SecretPassword = "P@ssw0rd!" | ConvertTo-SecureString -AsPlainText -Force
+        New-PSCredential -UserName "Admin" -Password $SecretPassword
+
+    #>
+    [OutputType([System.Management.Automation.PSCredential])]
+    [Cmdletbinding()]
+    param(
+        # The username of the PSCredential
+        [Parameter(Mandatory)]
+        [string] $UserName = (Read-Host -Prompt 'Enter a UserName'),
+
+        # The plain text password of the PSCredential
+        [Parameter(Mandatory)]
+        [ValidateScript({ ($_ -is [System.Security.SecureString]) -or ($_ -is [System.String]) })]
+        [object] $Password = (Read-Host -Prompt 'Enter Password' -AsSecureString)
+    )
+
+    try {
+        if ($Password -is [System.String]) {
+            $Password = $Password | ConvertTo-SecureString -AsPlainText -Force
+        }
+        $credential = New-Object -TypeName System.Management.Automation.PSCredential($UserName, $Password)
+    } catch {
+        throw $_.Exception.Message
+    }
+    return $credential
+}
---
+++ src/Utilities/public/PowerShell/PSCredential/Restore-PSCredential.ps1
@@ -0,0 +1,43 @@
+﻿
+function Restore-PSCredential {
+    <#
+        .SYNOPSIS
+        Restores a PSCredential from a file.
+
+        .DESCRIPTION
+        Takes in a UserName and restores a PSCredential from a file.
+
+        .EXAMPLE
+        Restore-PSCredential -UserName "Admin"
+
+        This restores the PSCredential from the default location of $env:HOMEPATH\.creds\Admin.cred
+
+        .EXAMPLE
+        Restore-PSCredential -UserName "Admin" -Path "C:\Temp"
+
+        This restores the PSCredential from the location of C:\Temp\Admin.cred
+    #>
+    [OutputType([System.Management.Automation.PSCredential])]
+    [CmdletBinding()]
+    param(
+        # The username of the PSCredential
+        [Parameter(Mandatory)]
+        [string] $UserName,
+
+        # The folder path to restore the PSCredential from.
+        [Parameter()]
+        [string] $Path = "$env:HOMEPATH\.creds"
+    )
+
+    $fileName = "$UserName.cred"
+    $credFilePath = Join-Path -Path $Path -ChildPath $fileName
+    $credFilePathExists = Test-Path $credFilePath
+
+    if ($credFilePathExists) {
+        $secureString = Get-Content $credFilePath | ConvertTo-SecureString
+        $credential = New-Object -TypeName System.Management.Automation.PSCredential($UserName, $secureString)
+    } else {
+        throw "Unable to locate a credential file for $($Username)"
+    }
+    return $credential
+}
