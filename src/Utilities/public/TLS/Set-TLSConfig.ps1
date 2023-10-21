ae84f4af22dff1a8f63312ce63846718bd09dea5
Marius Storhaug <marstor@hotmail.com>
2023-10-21 20:25:47 +0200

    Fix Set-TLSConfig

---
+++ src/Utilities/public/TLS/Set-TLSConfig.ps1
@@ -0,0 +1,26 @@
+﻿function Set-TLSConfig {
+    <#
+        .SYNOPSIS
+        Set the TLS configuration for the current PowerShell session
+
+        .DESCRIPTION
+        Set the TLS configuration for the current PowerShell session
+
+        .EXAMPLE
+        Set-TLSConfig -Protocol Tls12
+
+        Set the TLS configuration for the current PowerShell session to TLS 1.2
+    #>
+    [OutputType([void])]
+    [CmdletBinding()]
+    param(
+        # The TLS protocol to enable
+        [Parameter()]
+        [Net.SecurityProtocolType[]] $Protocol = [Net.SecurityProtocolType]::Tls12
+    )
+    
+    foreach ($protocolItem in $Protocol) {
+        Write-Verbose "Enabling $protocolItem"
+        [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor $protocolItem
+    }
+}
