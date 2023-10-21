173233fa61bd780c0960482c626cca0d6bb5a587
Marius Storhaug <marstor@hotmail.com>
2023-10-21 20:35:21 +0200

    Fix Reset-GitRepo

--- src/Utilities/public/Git/Reset-GitRepo.ps1
+++ src/Utilities/public/Git/Reset-GitRepo.ps1
@@ -1,12 +1,36 @@
-function Reset-Repo {
+function Reset-GitRepo {
+    <#
+        .SYNOPSIS
+        Reset a Git repository to the upstream branch
+
+        .DESCRIPTION
+        Reset a Git repository to the upstream branch
+
+        .EXAMPLE
+        Reset-GitRepo
+
+        Reset a Git repository to the upstream branch
+    #>
+    [OutputType([void])]
+    [CmdletBinding()]
     param(
-        $Upstream = 'upstream',
-        $Branch = 'main',
-        [bool]$Push = $true
+        # The upstream repository to reset to
+        [Parameter()]
+        [string] $Upstream = 'upstream',
+
+        # The branch to reset
+        [Parameter()]
+        [string] $Branch = 'main',
+
+        # Whether to push the reset
+        [Parameter()]
+        [switch] $Push
     )
+
     git fetch $Upstream
     git checkout $Branch
     git reset --hard $Upstream/$Branch
+
     if ($Push) {
         git push origin $Branch --force
     }
