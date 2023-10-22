7228e7a6a10b6edbc356f4b2120ce78598aa2510
Marius Storhaug <marstor@hotmail.com>
2023-10-22 22:30:25 +0200

    Test with testfiles from module

--- tests/Utilities/Casing.Tests.ps1
+++ tests/Utilities/Casing.Tests.ps1
@@ -1,6 +1,6 @@
 ﻿# Save the function Get-StringCasingStyle in a file named Get-StringCasingStyle.ps1
 # Then dot-source the file to load the function into the current session
-. .\Get-StringCasingStyle.ps1
+# . .\Get-StringCasingStyle.ps1
 
 # Define the Pester tests
 Describe 'Get-StringCasingStyle' {
