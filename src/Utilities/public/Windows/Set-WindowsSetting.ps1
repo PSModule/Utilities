11d19dacffb5dfee87300afb873485604cc8b358
Marius Storhaug <marstor@hotmail.com>
2024-01-29 00:13:30 +0100

    Add URI.Tests.ps1, Set-WindowsSetting.ps1, and Import-Variable.ps1 scripts. Remove Show-HiddenFiles.ps1 and Show-FileExtension.ps1 scripts.

---
+++ src/Utilities/public/Windows/Set-WindowsSetting.ps1
@@ -0,0 +1,35 @@
+﻿filter Set-WindowsSetting {
+    <#
+        .SYNOPSIS
+        Set a Windows setting
+
+        .DESCRIPTION
+        Set a or multiple Windows setting(s).
+
+        .NOTES
+        Supported OS: Windows
+    #>
+    [CmdletBinding(SupportsShouldProcess)]
+    Param (
+        # Show file extensions in Windows Explorer
+        [Parameter()]
+        [switch] $ShowFileExtension,
+
+        # Show hidden files in Windows Explorer
+        [Parameter()]
+        [switch] $ShowHiddenFiles
+    )
+
+    $path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'
+    if ($PSCmdlet.ShouldProcess("'ShowFileExtension' to [$ShowFileExtension]", 'Set')) {
+        Set-ItemProperty -Path $path -Name HideFileExt -Value ($ShowFileExtension ? 0 : 1)
+    }
+
+    if ($PSCmdlet.ShouldProcess("'ShowHiddenFiles' to [$ShowFileExtension]", 'Set')) {
+        Set-ItemProperty -Path $path -Name Hidden -Value ($ShowHiddenFiles ? 1 : 2)
+    }
+
+    # Refresh File Explorer
+    $Shell = New-Object -ComObject Shell.Application
+    $Shell.Windows() | ForEach-Object { $_.Refresh() }
+}
---
+++ tests/Utilities/URI.Tests.ps1
@@ -0,0 +1,15 @@
+﻿Describe "Join-URI" {
+    It "Join-Uri -Path 'https://example.com' -ChildPath 'foo' -AdditionalChildPath 'bar'" {
+        $uri = Join-Uri -Path 'https://example.com' -ChildPath 'foo' -AdditionalChildPath 'bar'
+        Write-Verbose $uri -Verbose
+        Write-Verbose "Should -Be 'https://example.com/foo/bar'" -Verbose
+        $uri | Should -Be 'https://example.com/foo/bar'
+    }
+
+    It "Join-Uri 'https://example.com/foo' '/bar/' '//baz/something/' '/test/'" {
+        $uri = Join-Uri 'https://example.com/foo' '/bar/' '//baz/something/' '/test/'
+        Write-Verbose $uri -Verbose
+        Write-Verbose "Should -Be 'https://example.com/foo/bar/baz/something/test'" -Verbose
+        $uri | Should -Be 'https://example.com/foo/bar/baz/something/test'
+    }
+}
