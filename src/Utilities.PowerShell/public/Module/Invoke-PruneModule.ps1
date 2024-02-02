fe34802b7c42b06b0347da332735ab8bc8007a26
Marius Storhaug <marstor@hotmail.com>
2024-02-02 17:50:53 +0100

    Publish Utilities.PowerShell

---
+++ src/Utilities.PowerShell/public/Module/Invoke-PruneModule.ps1
@@ -0,0 +1,58 @@
+﻿function Invoke-PruneModule {
+    <#
+        .SYNOPSIS
+        Remove all but the newest version of a module
+
+        .DESCRIPTION
+        Remove all but the newest version of a module
+
+        .EXAMPLE
+        Invoke-PruneModule -Name 'Az.*' -Scope CurrentUser
+    #>
+    [OutputType([void])]
+    [CmdletBinding()]
+    [Alias('Prune-Module')]
+    param (
+        # Name of the module(s) to prune
+        [Parameter()]
+        [string[]] $Name = '*',
+
+        # Scope of the module(s) to prune
+        [Parameter()]
+        [ValidateSet('CurrentUser', 'AllUsers')]
+        [string[]] $Scope = 'CurrentUser'
+    )
+
+    if ($Scope -eq 'AllUsers' -and -not (IsAdmin)) {
+        $message = 'Administrator rights are required to uninstall modules for all users. Please run the command again with' +
+        " elevated rights (Run as Administrator) or provide '-Scope CurrentUser' to your command."
+
+        throw $message
+    }
+
+    $UpdateableModules = Get-InstalledModule | Where-Object Name -Like "$Name"
+    $UpdateableModuleNames = $UpdateableModules.Name | Sort-Object -Unique
+    foreach ($UpdateableModuleName in $UpdateableModuleNames) {
+        $UpdateableModule = $UpdateableModules | Where-Object Name -EQ $UpdateableModuleName | Sort-Object -Property Version -Descending
+        Write-Verbose "[$($UpdateableModuleName)] - Found [$($UpdateableModule.Count)]" -Verbose
+
+        $NewestModule = $UpdateableModule | Select-Object -First 1
+        Write-Verbose "[$($UpdateableModuleName)] - Newest [$($NewestModule.Version -join ', ')]" -Verbose
+
+        $OutdatedModules = $UpdateableModule | Select-Object -Skip 1
+        Write-Verbose "[$($UpdateableModuleName)] - Outdated [$($OutdatedModules.Version -join ', ')]" -Verbose
+
+        foreach ($OutdatedModule in $OutdatedModules) {
+            Write-Verbose "[$($UpdateableModuleName)] - [$($OutdatedModule.Version)] - Removing" -Verbose
+            $OutdatedModule | Remove-Module -Force
+            Write-Verbose "[$($UpdateableModuleName)] - [$($OutdatedModule.Version)] - Uninstalling" -Verbose
+            Uninstall-Module -Name $OutdatedModule.Name -RequiredVersion -Force
+            try {
+                $OutdatedModule.ModuleBase | Remove-Item -Force -Recurse -ErrorAction Stop
+            } catch {
+                Write-Warning "[$($UpdateableModuleName)] - [$($OutdatedModule.Version)] - Failed to remove [$($OutdatedModule.ModuleBase)]"
+                continue
+            }
+        }
+    }
+}
---
+++ src/Utilities.PowerShell/public/Object/Copy-Object.ps1
@@ -0,0 +1,27 @@
+﻿filter Copy-Object {
+    <#
+        .SYNOPSIS
+        Copy an object
+
+        .DESCRIPTION
+        Copy an object
+
+        .EXAMPLE
+        $Object | Copy-Object
+
+        Copy an object
+    #>
+    [OutputType([object])]
+    [CmdletBinding()]
+    param (
+        # Object to copy
+        [Parameter(
+            Mandatory,
+            ValueFromPipeline
+        )]
+        [Object] $InputObject
+    )
+
+    $InputObject | ConvertTo-Json -Depth 100 | ConvertFrom-Json
+
+}
