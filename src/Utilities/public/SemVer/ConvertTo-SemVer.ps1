35c64137f83be90bb591e5784a4e144ced393054
Marius Storhaug <marstor@hotmail.com>
2024-02-10 22:19:52 +0100

    Added SemVer functions (#1)
    Added functions to handle semver conversion and new version.

---
+++ src/Utilities/public/SemVer/ConvertTo-SemVer.ps1
@@ -0,0 +1,50 @@
+﻿filter ConvertTo-SemVer {
+    <#
+        .SYNOPSIS
+        Converts a version string to a SemVer object.
+
+        .DESCRIPTION
+        This function takes a version string and converts it to a SemVer object.
+
+        .EXAMPLE
+        '1.2.3-alpha.1+001' | ConvertTo-SemVer
+
+        Major         : 1
+        Minor         : 2
+        Patch         : 3
+        Prerelease    : alpha.1
+        BuildMetadata : 001
+
+        .NOTES
+        Compatible with [SemVer 2.0.0](https://semver.org/).
+    #>
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+        'PSAvoidLongLines', '',
+        Justification = 'Long regex pattern'
+    )]
+    [CmdletBinding()]
+    param (
+        # The version to convert.
+        [Parameter(
+            Mandatory,
+            ValueFromPipeline,
+            ValueFromPipelineByPropertyName)
+        ]
+        [string] $Version
+    )
+
+    $semVerPattern = '^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$'
+
+    if ($Version -match $semVerPattern) {
+        [PSCustomObject]@{
+            Major         = [int]$Matches[1]
+            Minor         = [int]$Matches[2]
+            Patch         = [int]$Matches[3]
+            Prerelease    = $Matches[4]
+            BuildMetadata = $Matches[5]
+        }
+    } else {
+        Write-Verbose 'Invalid semver version format'
+        return $null
+    }
+}
---
+++ src/Utilities/public/SemVer/New-SemVer.ps1
@@ -0,0 +1,56 @@
+﻿Function New-SemVer {
+    <#
+        .SYNOPSIS
+        Creates a new SemVer object.
+
+        .DESCRIPTION
+        This function creates a new SemVer object.
+
+        .EXAMPLE
+        New-SemVer -Major 1 -Minor 2 -Patch 3 -Prerelease 'alpha.1' -BuildMetadata '001'
+
+        Major         : 1
+        Minor         : 2
+        Patch         : 3
+        Prerelease    : alpha.1
+        BuildMetadata : 001
+
+        .NOTES
+        Compatible with [SemVer 2.0.0](https://semver.org/).
+    #>
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+        'PSUseShouldProcessForStateChangingFunctions', '',
+        Justification = 'Does not change system state, but creates a new object.'
+    )]
+    [CmdletBinding()]
+    param (
+        # The major version.
+        [Parameter()]
+        [int] $Major = 0,
+
+        # The minor version.
+        [Parameter()]
+        [int] $Minor = 0,
+
+        # The patch version.
+        [Parameter()]
+        [int] $Patch = 0,
+
+        # The prerelease version.
+        [Parameter()]
+        [string] $Prerelease = '',
+
+        # The build metadata.
+        [Parameter()]
+        [string] $BuildMetadata = ''
+    )
+    process {
+        [PSCustomObject]@{
+            Major         = $Major
+            Minor         = $Minor
+            Patch         = $Patch
+            Prerelease    = $Prerelease
+            BuildMetadata = $BuildMetadata
+        }
+    }
+}
