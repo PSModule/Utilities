418e9d3fea67d269b5045b5be8f5a8054597a792
Marius Storhaug <marstor@hotmail.com>
2024-02-24 00:00:55 +0100

    Swap to debug for IsNullOrEmpty

--- src/Utilities/public/String/Test-IsNullOrEmpty.ps1
+++ src/Utilities/public/String/Test-IsNullOrEmpty.ps1
@@ -7,7 +7,7 @@
         Test if an object is null or empty
 
         .EXAMPLE
-        '' | IsNullOrEmpty -Verbose
+        '' | IsNullOrEmpty
 
         True
     #>
@@ -26,59 +26,59 @@
 
     try {
         if (-not ($PSBoundParameters.ContainsKey('Object'))) {
-            Write-Verbose 'Object was never passed, meaning its empty or null.'
+            Write-Debug 'Object was never passed, meaning its empty or null.'
             return $true
         }
         if ($null -eq $Object) {
-            Write-Verbose 'Object is null'
+            Write-Debug 'Object is null'
             return $true
         }
-        Write-Verbose "Object is: $($Object.GetType().Name)"
+        Write-Debug "Object is: $($Object.GetType().Name)"
         if ($Object -eq 0) {
-            Write-Verbose 'Object is 0'
+            Write-Debug 'Object is 0'
             return $true
         }
         if ($Object.Length -eq 0) {
-            Write-Verbose 'Object is empty array or string'
+            Write-Debug 'Object is empty array or string'
             return $true
         }
         if ($Object.GetType() -eq [string]) {
             if ([string]::IsNullOrWhiteSpace($Object)) {
-                Write-Verbose 'Object is empty string'
+                Write-Debug 'Object is empty string'
                 return $true
             } else {
-                Write-Verbose 'Object is not an empty string'
+                Write-Debug 'Object is not an empty string'
                 return $false
             }
         }
         if ($Object.Count -eq 0) {
-            Write-Verbose 'Object count is 0'
+            Write-Debug 'Object count is 0'
             return $true
         }
         if (-not $Object) {
-            Write-Verbose 'Object evaluates to false'
+            Write-Debug 'Object evaluates to false'
             return $true
         }
         if (($Object.GetType().Name -ne 'PSCustomObject')) {
-            Write-Verbose 'Casting object to PSCustomObject'
+            Write-Debug 'Casting object to PSCustomObject'
             $Object = [PSCustomObject]$Object
         }
         if (($Object.GetType().Name -eq 'PSCustomObject')) {
-            Write-Verbose 'Object is PSCustomObject'
+            Write-Debug 'Object is PSCustomObject'
             if ($Object -eq (New-Object -TypeName PSCustomObject)) {
-                Write-Verbose 'Object is similar to empty PSCustomObject'
+                Write-Debug 'Object is similar to empty PSCustomObject'
                 return $true
             }
             if (($Object.psobject.Properties).Count | Test-IsNullOrEmpty) {
-                Write-Verbose 'Object has no properties'
+                Write-Debug 'Object has no properties'
                 return $true
             }
         }
     } catch {
-        Write-Verbose 'Object triggered exception'
+        Write-Debug 'Object triggered exception'
         return $true
     }
 
-    Write-Verbose 'Object is not null or empty'
+    Write-Debug 'Object is not null or empty'
     return $false
 }
