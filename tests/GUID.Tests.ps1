012a2cd262535cc44eca0132a521f0afec036244
Marius Storhaug <marstor@hotmail.com>
2024-02-25 23:11:08 +0100

    🩹[Patch] Add tests for GUID functions (#3)

---
+++ tests/GUID.Tests.ps1
@@ -0,0 +1,13 @@
+﻿Describe "Search-GUID" {
+    It "Should return a GUID" {
+        $GUID = '123e4567-e89b-12d3-a456-426655440000' | Search-GUID
+        $GUID | Should -Be "123e4567-e89b-12d3-a456-426655440000"
+    }
+}
+
+Describe "Test-GUID" {
+    It "Should return a GUID" {
+        $GUID = '123e4567-e89b-12d3-a456-426655440000' | Test-IsGUID
+        $guid | Should -BeTrue
+    }
+}
