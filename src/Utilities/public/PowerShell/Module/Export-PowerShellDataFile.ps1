be836d05d4b39a212d4e53bfb65eb2ad699abc69
Marius Storhaug <marstor@hotmail.com>
2024-03-04 08:13:57 +0100

    Fix for Export-PowerShellDataFile (#13)

--- src/Utilities/public/PowerShell/Module/Export-PowerShellDataFile.ps1
+++ src/Utilities/public/PowerShell/Module/Export-PowerShellDataFile.ps1
@@ -14,6 +14,7 @@
         [switch] $Force
     )
 
-    $psd1Content = Convert-HashtableToString -Hashtable $Hashtable
-    $psd1Content | Out-File -FilePath $FilePath -Force:$Force
+    $content = Convert-HashtableToString -Hashtable $Hashtable
+    $content | Out-File -FilePath $Path -Force:$Force
+    Format-ModuleManifest -Path $Path
 }
