2d4baa26cc74dc9aed832e76c231e53d2871c829
Marius Storhaug <marstor@hotmail.com>
2024-03-04 23:46:25 +0100

    Custom Manifest function fixes (#19)

--- src/Utilities/public/PowerShell/Module/Get-ModuleManifest.ps1
+++ src/Utilities/public/PowerShell/Module/Get-ModuleManifest.ps1
@@ -9,7 +9,7 @@
         .EXAMPLE
         Get-PSModuleManifest -Path 'src/PSModule/PSModule.psd1' -As Hashtable
     #>
-    [OutputType([string], [System.IO.FileInfo], [System.Collections.Hashtable])]
+    [OutputType([string], [System.IO.FileInfo], [System.Collections.Hashtable], [ordered])]
     [CmdletBinding()]
     param(
         # Path to the module manifest file.
@@ -23,7 +23,7 @@
     )
 
     if (-not (Test-Path -Path $Path)) {
-        Write-Warning "No manifest file found."
+        Write-Warning 'No manifest file found.'
         return $null
     }
     Write-Verbose "Found manifest file [$Path]"
@@ -36,7 +36,85 @@
             return Get-Content -Path $Path
         }
         'Hashtable' {
-            return Import-PowerShellDataFile -Path $Path
+            $manifest = [ordered]@{}
+            $psData = [ordered]@{}
+            $privateData = [ordered]@{}
+
+            $tempManifest = Import-PowerShellDataFile -Path $Path
+            if ($tempManifest.ContainsKey('PrivateData')) {
+                $tempPrivateData = $tempManifest.PrivateData
+                if ($tempPrivateData.ContainsKey('PSData')) {
+                    $tempPSData = $tempPrivateData.PSData
+                    $tempPrivateData.Remove('PSData')
+                }
+            }
+
+            $psdataOrder = @(
+                'Tags'
+                'LicenseUri'
+                'ProjectUri'
+                'IconUri'
+                'ReleaseNotes'
+                'Prerelease'
+                'RequireLicenseAcceptance'
+                'ExternalModuleDependencies'
+            )
+            foreach ($key in $psdataOrder) {
+                if ($tempPSData.ContainsKey($key)) {
+                    $psData.$key = $tempPSData.$key
+                }
+            }
+
+            foreach ($key in $tempPrivateData.Keys) {
+                $privateData.$key = $tempPrivateData.$key
+            }
+            if ($psData.Count -gt 0) {
+                $privateData.PSData = $psData
+            }
+            $manifestOrder = @(
+                'RootModule'
+                'ModuleVersion'
+                'CompatiblePSEditions'
+                'GUID'
+                'Author'
+                'CompanyName'
+                'Copyright'
+                'Description'
+                'PowerShellVersion'
+                'PowerShellHostName'
+                'PowerShellHostVersion'
+                'DotNetFrameworkVersion'
+                'ClrVersion'
+                'ProcessorArchitecture'
+                'RequiredModules'
+                'RequiredAssemblies'
+                'ScriptsToProcess'
+                'TypesToProcess'
+                'FormatsToProcess'
+                'NestedModules'
+                'FunctionsToExport'
+                'CmdletsToExport'
+                'VariablesToExport'
+                'AliasesToExport'
+                'DscResourcesToExport'
+                'ModuleList'
+                'FileList'
+                'HelpInfoURI'
+                'DefaultCommandPrefix'
+                'PrivateData'
+            )
+            foreach ($key in $manifestOrder) {
+                if ($tempManifest.ContainsKey($key)) {
+                    $manifest.$key = $tempManifest.$key
+                }
+            }
+            if ($privateData.Count -gt 0) {
+                $manifest.PrivateData = $privateData
+            } else {
+                $manifest.Remove('PrivateData')
+            }
+
+            return $manifest
         }
     }
 }
