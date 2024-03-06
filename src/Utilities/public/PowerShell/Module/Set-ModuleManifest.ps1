1def0c49225f5a5c59e42467394465b024b485da
Marius Storhaug <marstor@hotmail.com>
2024-03-06 02:16:10 +0100

    working so far

--- src/Utilities/public/PowerShell/Module/Set-ModuleManifest.ps1
+++ src/Utilities/public/PowerShell/Module/Set-ModuleManifest.ps1
@@ -22,160 +22,198 @@
 
         #Script module or binary module file associated with this manifest.
         [Parameter()]
+        [AllowNull()]
         [string] $RootModule,
 
         #Version number of this module.
         [Parameter()]
+        [AllowNull()]
         [Version] $ModuleVersion,
 
         # Supported PSEditions.
         [Parameter()]
+        [AllowNull()]
         [string[]] $CompatiblePSEditions,
 
         # ID used to uniquely identify this module.
         [Parameter()]
+        [AllowNull()]
         [guid] $GUID,
 
         # Author of this module.
         [Parameter()]
+        [AllowNull()]
         [string] $Author,
 
         # Company or vendor of this module.
         [Parameter()]
+        [AllowNull()]
         [string] $CompanyName,
 
         # Copyright statement for this module.
         [Parameter()]
+        [AllowNull()]
         [string] $Copyright,
 
         # Description of the functionality provided by this module.
         [Parameter()]
+        [AllowNull()]
         [string] $Description,
 
         # Minimum version of the PowerShell engine required by this module.
         [Parameter()]
+        [AllowNull()]
         [Version] $PowerShellVersion,
 
         # Name of the PowerShell host required by this module.
         [Parameter()]
+        [AllowNull()]
         [string] $PowerShellHostName,
 
         # Minimum version of the PowerShell host required by this module.
         [Parameter()]
+        [AllowNull()]
         [version] $PowerShellHostVersion,
 
         # Minimum version of Microsoft .NET Framework required by this module.
         # This prerequisite is valid for the PowerShell Desktop edition only.
         [Parameter()]
+        [AllowNull()]
         [Version] $DotNetFrameworkVersion,
 
         # Minimum version of the common language runtime (CLR) required by this module.
         # This prerequisite is valid for the PowerShell Desktop edition only.
         [Parameter()]
+        [AllowNull()]
         [Version] $ClrVersion,
 
         # Processor architecture (None,X86, Amd64) required by this module
         [Parameter()]
+        [AllowNull()]
         [System.Reflection.ProcessorArchitecture] $ProcessorArchitecture,
 
         # Modules that must be imported into the global environment prior to importing this module.
         [Parameter()]
+        [AllowNull()]
         [Object[]] $RequiredModules,
 
         # Assemblies that must be loaded prior to importing this module.
         [Parameter()]
+        [AllowNull()]
         [string[]] $RequiredAssemblies,
 
         # Script files (.ps1) that are run in the caller's environment prior to importing this module.
         [Parameter()]
+        [AllowNull()]
         [string[]] $ScriptsToProcess,
 
         # Type files (.ps1xml) to be loaded when importing this module.
         [Parameter()]
+        [AllowNull()]
         [string[]] $TypesToProcess,
 
         # Format files (.ps1xml) to be loaded when importing this module.
         [Parameter()]
+        [AllowNull()]
         [string[]] $FormatsToProcess,
 
         # Modules to import as nested modules of the module specified in RootModule/ModuleToProcess.
         [Parameter()]
+        [AllowNull()]
         [Object[]] $NestedModules,
 
         # Functions to export from this module, for best performance, do not use wildcards and do not
         # delete the entry, use an empty array if there are no functions to export.
         [Parameter()]
+        [AllowNull()]
         [string[]] $FunctionsToExport,
 
         # Cmdlets to export from this module, for best performance, do not use wildcards and do not
         # delete the entry, use an empty array if there are no cmdlets to export.
         [Parameter()]
+        [AllowNull()]
         [string[]] $CmdletsToExport,
 
         # Variables to export from this module.
         [Parameter()]
+        [AllowNull()]
         [string[]] $VariablesToExport,
 
         # Aliases to export from this module, for best performance, do not use wildcards and do not
         # delete the entry, use an empty array if there are no aliases to export.
         [Parameter()]
+        [AllowNull()]
         [string[]] $AliasesToExport,
 
         # DSC resources to export from this module.
         [Parameter()]
+        [AllowNull()]
         [string[]] $DscResourcesToExport,
 
         # List of all modules packaged with this module.
         [Parameter()]
+        [AllowNull()]
         [Object[]] $ModuleList,
 
         # List of all files packaged with this module.
         [Parameter()]
+        [AllowNull()]
         [string[]] $FileList,
 
         # Tags applied to this module. These help with module discovery in online galleries.
         [Parameter()]
+        [AllowNull()]
         [string[]] $Tags,
 
         # A URL to the license for this module.
         [Parameter()]
+        [AllowNull()]
         [uri] $LicenseUri,
 
         # A URL to the main website for this project.
         [Parameter()]
+        [AllowNull()]
         [uri] $ProjectUri,
 
         # A URL to an icon representing this module.
         [Parameter()]
+        [AllowNull()]
         [uri] $IconUri,
 
         # ReleaseNotes of this module.
         [Parameter()]
+        [AllowNull()]
         [string] $ReleaseNotes,
 
         # Prerelease string of this module.
         [Parameter()]
+        [AllowNull()]
         [string] $Prerelease,
 
         # Flag to indicate whether the module requires explicit user acceptance for install/update/save.
         [Parameter()]
+        [AllowNull()]
         [bool] $RequireLicenseAcceptance,
 
         # External dependent modules of this module.
         [Parameter()]
+        [AllowNull()]
         [string[]] $ExternalModuleDependencies,
 
         # HelpInfo URI of this module.
         [Parameter()]
+        [AllowNull()]
         [String] $HelpInfoURI,
 
         # Default prefix for commands exported from this module. Override the default prefix using Import-Module -Prefix.
         [Parameter()]
+        [AllowNull()]
         [string] $DefaultCommandPrefix,
 
         # Private data to pass to the module specified in RootModule/ModuleToProcess.
         # This may also contain a PSData hashtable with additional module metadata used by PowerShell.
         [Parameter()]
+        [AllowNull()]
         [object] $PrivateData
     )
 
@@ -206,7 +244,11 @@
             $outPSData.$key = $tempPSData.$key
         }
         if ($PSBoundParameters.Keys.Contains($key)) {
-            $outPSData.$key = $PSBoundParameters.$key
+            if ($null -eq $PSBoundParameters.$key) {
+                $outPSData.Remove($key)
+            } else {
+                $outPSData.$key = $PSBoundParameters.$key
+            }
         }
     }
 
@@ -215,6 +257,8 @@
     }
     if ($outPSData.Count -gt 0) {
         $outPrivateData.PSData = $outPSData
+    } else {
+        $outPrivateData.Remove('PSData')
     }
     foreach ($key in $PrivateData.Keys) {
         $outPrivateData.$key = $PrivateData.$key
@@ -257,7 +301,11 @@
             $outManifest.$key = $tempManifest.$key
         }
         if ($PSBoundParameters.Keys.Contains($key)) {
-            $outManifest.$key = $PSBoundParameters.$key
+            if ($null -eq $PSBoundParameters.$key) {
+                $outManifest.Remove($key)
+            } else {
+                $outManifest.$key = $PSBoundParameters.$key
+            }
         }
     }
     if ($outPrivateData.Count -gt 0) {
