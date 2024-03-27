1434dd5b9e24d7ea92f62dfe392a70283c3c42ef
Marius Storhaug <marstor@hotmail.com>
2024-03-27 20:42:24 +0100

    test

--- tests/PowerShell.Module.Tests.ps1
+++ tests/PowerShell.Module.Tests.ps1
@@ -38,7 +38,7 @@
         $test.FunctionsToExport | Should -Contain 'Describe'
         $test.CmdletsToExport | Should -Be ''
         $test.VariablesToExport | Should -Be @()
-        $test.FormatsToProcess | Should -Contain 'Pester.Format.ps1xml'
+        $test.FormatsToProcess | Should -Contain  @()
         $test.PrivateData.PSData.Category | Should -Be 'Scripting Techniques'
         $test.PrivateData.PSData.Tags | Should -Contain 'bdd'
         $test.PrivateData.PSData.IconUri | Should -Be 'https://raw.githubusercontent.com/pester/Pester/main/images/pester.PNG'
