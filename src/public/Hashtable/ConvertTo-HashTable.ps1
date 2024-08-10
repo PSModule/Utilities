3a86192efd6d468ed31151166a18e02071621036
Marius Storhaug <marstor@hotmail.com>
2024-08-11 00:34:56 +0200

    ConvertTo-Hashtable

---
+++ src/public/Hashtable/ConvertTo-HashTable.ps1
@@ -0,0 +1,38 @@
+﻿function ConvertTo-Hashtable {
+    param (
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [PSObject]$InputObject
+    )
+
+    process {
+        if ($InputObject -is [System.Collections.IEnumerable] -and $InputObject -notlike "*") {
+            $array = @()
+            foreach ($item in $InputObject) {
+                $array += ConvertTo-Hashtable -InputObject $item
+            }
+            return $array
+        } elseif ($InputObject.PSObject.Properties.Name.Count -gt 0) {
+            $hashtable = @{}
+            foreach ($property in $InputObject.PSObject.Properties) {
+                $hashtable[$property.Name] = ConvertTo-Hashtable -InputObject $property.Value
+            }
+            return $hashtable
+        } else {
+            return $InputObject
+        }
+    }
+}
+
+$json = '{
+    "key1": "value1",
+    "key2": {
+        "nestedKey1": "nestedValue1",
+        "nestedKey2": [1, 2, 3]
+    },
+    "key3": ["item1", "item2"]
+}'
+
+$object = $json | ConvertFrom-Json
+$hashtable = ConvertTo-Hashtable -InputObject $object
+
+$hashtable
