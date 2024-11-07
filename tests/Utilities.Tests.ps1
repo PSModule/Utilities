d957f8e7f6a3089fff72c6ff6711d2b6655130be
Marius Storhaug <marstor@hotmail.com>
2024-11-07 23:35:40 +0100

    remove paths

--- tests/Utilities.Tests.ps1
+++ tests/Utilities.Tests.ps1
@@ -2,13 +2,7 @@
     'PSAvoidUsingConvertToSecureStringWithPlainText', '', Justification = 'Test code only'
 )]
 [CmdletBinding()]
-Param(
-    # Path to the module to test.
-    [Parameter()]
-    [string] $Path
-)
-
-Write-Verbose "Path to the module: [$Path]" -Verbose
+param()
 
 Describe 'Utilities' {
     Describe 'Function: Join-URI' {
