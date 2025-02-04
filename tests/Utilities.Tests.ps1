274e034e5b85c904f85802ff77be18f4f7901e35
Marius Storhaug <marstor@hotmail.com>
2025-02-04 13:15:10 +0100

    🪲 [Fix]: Remove deprecated tests for `Join-Uri` function

--- tests/Utilities.Tests.ps1
+++ tests/Utilities.Tests.ps1
@@ -5,22 +5,6 @@
 param()
 
 Describe 'Utilities' {
-    Describe 'Function: Join-URI' {
-        It "Join-Uri -Path 'https://example.com' -ChildPath 'foo' -AdditionalChildPath 'bar'" {
-            $uri = Join-Uri -Path 'https://example.com' -ChildPath 'foo' -AdditionalChildPath 'bar'
-            Write-Verbose $uri -Verbose
-            Write-Verbose "Should -Be 'https://example.com/foo/bar'" -Verbose
-            $uri | Should -Be 'https://example.com/foo/bar'
-        }
-
-        It "Join-Uri 'https://example.com/foo' '/bar/' '//baz/something/' '/test/'" {
-            $uri = Join-Uri 'https://example.com/foo' '/bar/' '//baz/something/' '/test/'
-            Write-Verbose $uri -Verbose
-            Write-Verbose "Should -Be 'https://example.com/foo/bar/baz/something/test'" -Verbose
-            $uri | Should -Be 'https://example.com/foo/bar/baz/something/test'
-        }
-    }
-
     Describe 'Function: IsNullOrEmpty' {
         It 'Returns true for $null' {
             ($null | IsNullOrEmpty -Verbose) | Should -BeTrue
